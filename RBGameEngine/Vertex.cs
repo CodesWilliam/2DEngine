8cbecc669e2478d0b193fc2d75b0f629f3c13674
Idanord <33676867+Idanord@users.noreply.github.com>
2018-08-20 02:56:18 -0700

    Mesh2D
    declars what type of buffer is to be used on vboID in this case it is an ArrayBuffer type for the target of the buffer. Added Vertex.cs that sets position of vectors.

---
+++ RBGameEngine/Vertex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK;
+
+namespace RBGameEngine
+{
+    public class Vertex
+    {
+
+        private const int VertexSize = 2;
+        private Vector2 position;
+
+        //Properties
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Vertex(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public static float[] Process(Vertex[] vertices)
+        {
+            float[] data = new float[vertices.Length * VertexSize];  //multiplying by 2 due to having 2 types in Vector2
+
+            for(int i = 0; i < vertices.Length; i++)
+            {
+                data[i] = vertices[i].Position.X;
+                data[i++] = vertices[i].Position.Y;
+            }
+
+            return data;
+        }
+    }
+}
